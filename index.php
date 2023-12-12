6e068c48e089d87029ce529af44328f3c94f5805
daonhan2004 <151822154+daonhan2004@users.noreply.github.com>
2023-12-12 16:50:27 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,268 @@
+<?php
+$string =  '<!DOCTYPE html>'.
+'<html lang="en">'.
+'<head>'.
+'    <meta charset="UTF-8">'.
+'    <meta name="viewport" content="width=device-width, initial-scale=1.0">'.
+'    <title>Du lịch</title>'.
+''.
+'    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />'.
+''.
+
+'    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">'.
+''.
+'<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">'.
+'    <link rel="stylesheet" href="style.css">'.
+''.
+'</head>'.
+'<body>'.
+'    '.
+''.
+'<header>'.
+''.
+'    <div id="menu-bar" class="fas fa-bars"></div>'.
+''.
+'    <a href="#" class="logo"><span>T</span>ravel</a>'.
+''.
+'    <nav class="navbar">.'.
+'        <a href="./index.php">Trang chủ</a>'.
+'       <a href="./gioithieu.php?type=gioithieu">Giới thiệu</a>'.
+'       <a href="./tour.php?type=tour">Tour</a>'.
+'        <a href="./dichvu.php?type=dichvu">Dịch vụ</a>'.
+'        <a href="./tintuc.php?type=tintuc">Tin tức</a>'.
+'        <a href="./lienhe.php?type=lienhe">Liên hệ</a>'.
+'        <a href="./admin/index.php" target="_blank">Admin</a>'.
+'    </nav>'.
+''.
+'    <div class="icons">'.
+'        <i class="fas fa-search" id="search-btn"></i>'.
+'        <i class="fas fa-user" id="login-btn"></i>'.
+'    </div>'.
+''.
+'    <form action="" class="search-bar-container">'.
+'        <input type="search" id="search-bar" placeholder="search here...">'.
+'        <label for="search-bar" class="fas fa-search"></label>'.
+'    </form>'.
+''.
+'</header>'.
+''.
+''.
+'<div class="login-form-container">'.
+''.
+'    <i class="fas fa-times" id="form-close"></i>'.
+''.
+'    <form action="">'.
+'        <h3>login</h3>'.
+'        <input type="email" class="box" placeholder="enter your email">'.
+'        <input type="password" class="box" placeholder="enter your password">'.
+'        <input type="submit" value="login now" class="btn">'.
+'        <input type="checkbox" id="remember">'.
+'        <label for="remember">remember me</label>'.
+'        <p>forget password? <a href="#">click here</a></p>'.
+'        <p>don\'t have and account? <a href="#">register now</a></p>'.
+'    </form>'.
+''.
+'</div>'.
+''.
+''.
+'<section class="home" id="home">'.
+''.
+'    <div class="content">'.
+'        <h3>Chào mừng đến với Tân Sơn</h3>'.
+'        '.
+'    </div>'.
+''.
+'    '.
+''.
+'    <div class="video-container">'.
+'        <video src="images/vid-1.mp4" id="video-slider" loop autoplay muted></video>'.
+'    </div>'.
+''.
+'</section>'.
+''.
+'<section class="packages" id="packages">'.
+''.
+'    <h1 class="heading">'.
+'        <span>đ</span>'.
+'        <span>ồ</span>'.
+'        <span>i</span>'.
+'        <span>c</span>'.
+'        <span>h</span>'.
+'        <span>è</span>'.
+'        <span>L</span>'.
+'        <span>o</span>'.
+'        <span>n</span>'.
+'        <span>g</span>'.
+'        <span>C</span>'.
+'        <span>ố</span>'.
+'        <span>c</span>'.
+'    </h1>'.
+''.
+'    <div class="box-container">'.
+'        <h2 class="tieu-de">Tạm Quên Đồi Chè Cầu Đất Đà Lạt Đi, Phú Thọ Có Đồi Chè Long Sơn Đẹp Chẳng Kém Năm 2023</h2>'.
+'        <p>Mỗi khi nhắc về những đồi chè, bao người vẫn thường nhớ về những đồi chè nổi tiếng, như là đồi chè Cầu Đất ở Đà Lạt, đồi chè Thái Nguyên hay đồi chè Mộc Châu,… Thế nhưng không chỉ có những nơi đó là có đồi chè, mà ngay ở vùng đất tổ Phú Thọ cũng có những đồi chè xanh mướt, không chỉ mang đến những giá trị kinh tế cho người dân địa phương mà còn vẽ nên bức tranh đẹp nên thơ cho vùng đất này. Chuyến du lịch Phú Thọ dịp sắp tới, hãy ghé qua đồi chè Long Cốc và thưởng thức vẻ đẹp diệu kỳ của đồi chè nơi đây bạn nhé!</p>'.
+'        <p>Đồi chè Long Cốc thuộc địa phận xóm Măng 1, xã Long Cốc, huyện Tân Sơn, tỉnh Phú Thọ và cách thủ đô Hà Nội khoảng tầm 125 km về phía Tây Bắc và khoảng 70 km từ trung tâm thành phố Việt Trì. Từ trung tâm thủ đô, bạn có thể di chuyển đến đồi chè Long Cốc bằng xe máy hoặc chọn xe khách. Để có thể đến được nơi này, bạn có thể kết hợp sử dụng bản đồ trên điện thoại và hỏi người dân địa phương đường đến đồi chè xóm Măng 1.</p>'.
+'        <img src="./images/anh2.png" alt="" class="img-container">'.
+'<span class="des-img">Đồi chè Long Cốc - điểm đến mới lạ và hấp dẫn tại Đất Tổ Phú Thọ.</span>'.
+'        <p>Đồi chè Long Cốc đẹp chẳng kém cạnh bất cứ đồi chè nơi đâu. Khi được tận mắt ngắm nhìn vẻ đẹp của đồi chè, bạn sẽ ngỡ như được đắm mình trong không gian của một tác phẩm hội họa đẹp mê hồn, ngỡ như được lạc đến xứ sở cổ tích xa xôi nào đấy hay chốn bồng lai tiên cảnh giữa trần gian này. Đồi chè Long Cốc chính là điểm đến mà ngay từ cái nhìn đầu tiên, từ bước chân đầu tiên thôi, con tim bạn đã thuộc về nơi này.</p>'.
+'        <img src="./images/anh3.png" alt="" class="img-container">'.
+'<span class="des-img">Vào những ngày cuối thu đầu đông, đồi chè Long Cốc ở Tân Sơn ẩn hiện trong màn sương như chốn bồng lai tiên cảnh.</span>'.
+'    </div>  '.
+''.
+'</section>'.
+''.
+'<br>'.
+'<hr>'.
+'<section class="tintuc" id="tintuc">'.
+''.
+'    <h1 class="heading">'.
+'        <span>t</span>'.
+'        <span>r</span>'.
+'        <span>ư</span>'.
+'        <span>n</span>'.
+'        <span>g</span>'.
+'        <span>b</span>'.
+'        <span>à</span>'.
+'        <span>y</span>'.
+'    </h1>'.
+''.
+'    <div class="box-container">'.
+''.
+'<div class="col-12 col-md-12">'.
+'<div class="row">'.
+'        <div class="box col-4 col-md-4">'.
+'            <img src="images/a1.jpg" alt="">'.
+'            <div class="content">'.
+'            </div>'.
+'        </div>'.
+'        <div class="box col-4 col-md-4">'.
+'            <img src="images/a2.jpg" alt="">'.
+'            <div class="content">'.
+'            </div>'.
+'        </div>'.
+'        <div class="box col-4 col-md-4">'.
+'            <img src="images/a3.jpg" alt="">'.
+'            <div class="content">'.
+'            </div>'.
+'        </div>'.
+'        <div class="box col-4 col-md-4">'.
+'            <img src="images/a4.jpg" alt="">'.
+'            <div class="content">'.
+'            </div>'.
+'        </div>'.
+'        <div class="box col-4 col-md-4">'.
+'            <img src="images/11.jpg" alt="">'.
+'            <div class="content">'.
+'            </div>'.
+'        </div>'.
+'        <div class="box col-4 col-md-4">'.
+'            <img src="images/a6.jpg" alt="">'.
+'            <div class="content">'.
+'            </div>'.
+'        </div>'.
+'        <div class="box col-4 col-md-4">'.
+'            <img src="images/a7.jpg" alt="">'.
+'            <div class="content">'.
+'            </div>'.
+'        </div>'.
+'        <div class="box col-4 col-md-4">'.
+'            <img src="images/12.jpg" alt="">'.
+'            <div class="content">'.
+'            </div>'.
+'        </div>'.
+'        <div class="box col-4 col-md-4">'.
+'            <img src="images/a5.jpg" alt="">'.
+'            <div class="content">'.
+'            </div>'.
+'        </div>'.
+''.
+'</div>'.
+'</div>'.
+'    </div>'.
+''.
+'</section>'.
+''.
+'<br>'.
+'<hr>'.
+'<section class="contact" id="contact">'.
+'    '.
+'    <h1 class="heading">'.
+'        <span>l</span>'.
+'        <span>i</span>'.
+'        <span>ê</span>'.
+'        <span>n</span>'.
+'        <span>h</span>'.
+'        <span>ệ</span>'.
+'    </h1>'.
+''.
+'    <div class="row">'.
+''.
+'        <div class="image">'.
+'            <img src="images/contact-img.svg" alt="">'.
+'        </div>'.
+''.
+'        <form action="">'.
+'            <div class="inputBox">'.
+'                <input type="text" placeholder="name">'.
+'                <input type="email" placeholder="email">'.
+'            </div>'.
+'            <div class="inputBox">'.
+'                <input type="number" placeholder="number">'.
+'                <input type="text" placeholder="subject">'.
+'            </div>'.
+'            <textarea placeholder="message" name="" id="" cols="30" rows="10"></textarea>'.
+'            <input type="submit" class="btn" value="send message">'.
+'        </form>'.
+''.
+'    </div>'.
+'    '.
+'</section>'.
+''.
+''.
+'<section class="footer">'.
+''.
+'    <div class="box-container">'.
+''.
+'        <div class="box">'.
+'            <h3>about us</h3>'.
+'            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quas magni pariatur est accusantium voluptas enim nemo facilis sit debitis.</p>'.
+'        </div>'.
+'        <div class="box">'.
+'            <h3>branch locations</h3>'.
+'            <a href="#">india</a>'.
+'            <a href="#">USA</a>'.
+'            <a href="#">japan</a>'.
+'            <a href="#">france</a>'.
+'        </div>'.
+'        <div class="box">'.
+'            <h3>quick links</h3>'.
+'            <a href="#">home</a>'.
+'            <a href="#">book</a>'.
+'            <a href="#">packages</a>'.
+'            <a href="#">services</a>'.
+'            <a href="#">gallery</a>'.
+'            <a href="#">review</a>'.
+'            <a href="#">contact</a>'.
+'        </div>'.
+'        <div class="box">'.
+'            <h3>follow us</h3>'.
+'            <a href="#">facebook</a>'.
+'            <a href="#">instagram</a>'.
+'            <a href="#">twitter</a>'.
+'            <a href="#">linkedin</a>'.
+'        </div>'.
+''.
+'    </div>'.
+''.
+'    <h1 class="credit"> created by <span> mr. web designer </span> | all rights reserved! </h1>'.
+''.
+'</section>'.
+''.
+'<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>'.
+'<script src="script.js"></script>'.
+''.
+'</body>'.
+'</html>';
+
+ echo $string; 
+?>
